a295b48e68fc4ab4ff41a9ed89cf3bb75f338aa9
809917187 <809917187@qq.com>
2024-12-02 21:13:18 +0800

    merge 2 branch

---
+++ IAMS/Controllers/ElectricityReportController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IAMS.Controllers {
+    public class ElectricityReportController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
